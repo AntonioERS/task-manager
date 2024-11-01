63dd123e78944aacd642b201ca25ee7f79eb783c
AntonioERS <antonioers@gmail.com>
2024-11-01 19:24:28 +0100

    Actualizados nombres botones

--- js/notificaciones.js
+++ js/notificaciones.js
@@ -1,15 +1,12 @@
-function mensaje1() {
+function botonsalir() {
   alert ("Mensaje de prueba 1");
 }
 
-function mensaje2() {
-  alert ("Mensaje de prueba 2");
+function botonmensajenuevo() {
+  alert ("You have mail!");
 }
 
-function mensaje3() {
+function botonentrar mensaje() {
   alert ("Mensaje de prueba 3");
 }
 
-function mensaje4() {
-  alert ("Mensaje de prueba 4");
-}

50a62a8127c0253b3ba54b632f74289a75d8f715
AntonioERS <antonioers@gmail.com>
2024-11-01 18:14:59 +0100

    Add files via upload

---
+++ js/notificaciones.js
@@ -0,0 +1,15 @@
+function mensaje1() {
+  alert ("Mensaje de prueba 1");
+}
+
+function mensaje2() {
+  alert ("Mensaje de prueba 2");
+}
+
+function mensaje3() {
+  alert ("Mensaje de prueba 3");
+}
+
+function mensaje4() {
+  alert ("Mensaje de prueba 4");
+}
